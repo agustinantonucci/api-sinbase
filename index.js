aee0588ff7ba6ac9eae379625be8e00d430f3ec5
agustinantonucci <agustinantonucci@gmail.com>
2022-09-19 20:32:58 -0300

    variables entorno api

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const api = require('./routes/api');
-import {PORT} from "./config.js";
+// import {PORT} from './config.js';
 
 app.use(express.json());
 
 app.use('/api', api);
 
-// const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on Port: ${PORT}`));
+const port = 6331 || 5000;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
 
--- models/db.js
+++ models/db.js
@@ -4,10 +4,10 @@ import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } from "../config.js";
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME
+  host: "containers-us-west-51.railway.app",
+  user: "root",
+  password: "WsOtCVzI10pVajOVtuz8",
+  database: "railway"
 });
 // open the MySQL connection
 connection.connect(error => {
