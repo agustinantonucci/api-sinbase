d18e75b122944453d357258af81f5a57da4c711b
agustinantonucci <agustinantonucci@gmail.com>
2022-09-26 17:54:07 -0300

    cambio de puerto

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-const express = require('express');
-const {getAll} = require("./controllers/medicoController.js");
+const express = require("express");
+const { getAll } = require("./controllers/medicoController.js");
 const app = express();
 // import {PORT} from './config.js';
 
 app.use(express.json());
 
 app.get("/", async function (request, response) {
+  try {
+    const [result] = await getAll();
 
-    try {
-  
-      const [result] = await getAll()
-  
-      response.send({ success: true, result })
-  
-    } catch (error) {
-  
-      response.status(500).send({
-  
-        success: false,
-  
-        error: "Algo salió mal",
-  
-      })
-  
-    }
-  
-  })
+    response.send({ success: true, result });
+  } catch (error) {
+    response.status(500).send({
+      success: false,
 
-const port = 6331 || 5000;
+      error: "Algo salió mal",
+    });
+  }
+});
+
+const port = 5000 || 6331;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
-
